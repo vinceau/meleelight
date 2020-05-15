d941e13c48ec1cdb57fa2489f22820af8ed379e9
Vince Au <vinceau09@gmail.com>
2020-05-15 23:30:02 +1000

    Remove keyboard polling

--- src/input/input.js
+++ src/input/input.js
@@ -8,7 +8,6 @@ import {buttonState, triggerValue, stickValue, dPadState } from "./gamepad/retri
 import {scaleToGCTrigger, scaleToMeleeAxes, tasRescale, deaden} from "./meleeInputs";
 
 import type {GamepadInfo} from "./gamepad/gamepadInfo";
-import {replayActive, retrieveReplayInputs} from "../main/replay";
 
 type Input = { a : bool
                     , b : bool
@@ -119,12 +118,10 @@ export function pollInputs ( gameMode : number, frameByFrame : bool, controllerI
                            , playerSlot : number, controllerIndex : number, keys : {[key: number] : bool}, playertype : number ) : Input {
    // input is the input for player i in the current frame
     let input = nullInput(); // initialise with default values
-    if(replayActive){
-      input = pollReplayInputs(gameMode, controllerInfo, playerSlot, controllerIndex, frameByFrame);
-    }else if(playertype === 1 && gameMode === 3 ){
+    if (playertype === 1 && gameMode === 3 ){
       return aiInputBank[playerSlot][0];
     }else if (controllerInfo === "keyboard") { // keyboard controls
-      input = pollKeyboardInputs(gameMode, frameByFrame, keys);
+      // do nothing
     }else if (playertype === 0) {
       input = pollGamepadInputs(gameMode, controllerInfo, playerSlot, controllerIndex, frameByFrame);
     }
@@ -132,101 +129,6 @@ export function pollInputs ( gameMode : number, frameByFrame : bool, controllerI
     console.log(input);
     return input;
   }
-
-function pollReplayInputs(gameMode, controllerType, playerSlot, controllerIndex, frameByFrame) {
-  return retrieveReplayInputs(playerSlot,controllerIndex);
-}
-
-function pollKeyboardInputs(gameMode : number, frameByFrame : bool, keys : {[key: number] : bool}) : Input {
-  const input = nullInput(); // initialise with default values
-
-  let stickR = 1;
-  let stickL = 1;
-  let stickU = 1;
-  let stickD = 1;
-  if (gameMode === 3 || gameMode === 5) {
-    stickR = keyMap.lstick.ranges[1];
-    stickL = keyMap.lstick.ranges[2];
-    stickU = keyMap.lstick.ranges[0];
-    stickD = keyMap.lstick.ranges[3];
-  }
-  let lstickX = (keys[keyMap.lstick.right[0]] || keys[keyMap.lstick.right[1]]) ? ((keys[keyMap.lstick.left[0]] ||
-    keys[keyMap.lstick.left[1]]) ? 0 : stickR) : ((keys[keyMap.lstick.left[0]] || keys[keyMap.lstick.left[1]]) ?
-    -stickL : 0);
-  let lstickY = (keys[keyMap.lstick.up[0]] || keys[keyMap.lstick.up[1]]) ? ((keys[keyMap.lstick.down[0]] || keys[
-    keyMap.lstick.down[1]]) ? 0 : stickU) : ((keys[keyMap.lstick.down[0]] || keys[keyMap.lstick.down[1]]) ? -
-    stickD : 0);
-
-  let lAnalog = (keys[keyMap.shoulders.lAnalog[0]] || keys[keyMap.shoulders.lAnalog[1]]) ? keyMap.shoulders.ranges[
-    0] : 0;
-  let rAnalog = (keys[keyMap.shoulders.rAnalog[0]] || keys[keyMap.shoulders.rAnalog[1]]) ? keyMap.shoulders.ranges[
-    1] : 0;
-  if (gameMode === 3 || gameMode === 5) {
-    for (let j = 0; j < 5; j++) {
-      if (keys[keyMap.lstick.modifiers[j][0]]) {
-        lstickX *= keyMap.lstick.modifiers[j][1];
-        lstickY *= keyMap.lstick.modifiers[j][2];
-      }
-      if (keys[keyMap.shoulders.modifiers[j][0]]) {
-        lAnalog *= keyMap.shoulders.modifiers[j][1];
-        rAnalog *= keyMap.shoulders.modifiers[j][2];
-      }
-    }
-  }
-  lstickX = Math.sign(lstickX) * Math.min(1, Math.abs(lstickX));
-  lstickY = Math.sign(lstickY) * Math.min(1, Math.abs(lstickY));
-  lAnalog = Math.min(1, Math.abs(lAnalog));
-  rAnalog = Math.min(1, Math.abs(rAnalog));
-
-  const cstickX = (keys[keyMap.cstick.right[0]] || keys[keyMap.cstick.right[1]]) ? ((keys[keyMap.cstick.left[0]] ||
-    keys[keyMap.cstick.left[1]]) ? 0 : 1) : ((keys[keyMap.cstick.left[0]] || keys[keyMap.cstick.left[1]]) ? -1 :
-    0);
-  const cstickY = (keys[keyMap.cstick.up[0]] || keys[keyMap.cstick.up[1]]) ? ((keys[keyMap.cstick.down[0]] || keys[
-    keyMap.cstick.down[1]]) ? 0 : 1) : ((keys[keyMap.cstick.down[0]] || keys[keyMap.cstick.down[1]]) ? -1 : 0);
-
-  const rescaledLStick = tasRescale(lstickX, lstickY, true);
-  input.lsX = deaden(rescaledLStick[0]);
-  input.lsY = deaden(rescaledLStick[1]);
-  input.rawX = rescaledLStick[0];
-  input.rawY = rescaledLStick[1];
-  const rescaledCStick = tasRescale(cstickX, cstickY, true);
-  input.csX = deaden(rescaledCStick[0]);
-  input.csY = deaden(rescaledCStick[1]);
-  input.rawcsX = rescaledCStick[0];
-  input.rawcsY = rescaledCStick[1];
-  input.lA  = lAnalog;
-  input.rA  = rAnalog;
-  input.s   = keys[keyMap.s[0]] || keys[keyMap.s[1]];
-  input.x   = keys[keyMap.x[0]] || keys[keyMap.x[1]];
-  input.a   = keys[keyMap.a[0]] || keys[keyMap.a[1]];
-  input.b   = keys[keyMap.b[0]] || keys[keyMap.b[1]];
-  input.y   = keys[keyMap.y[0]] || keys[keyMap.y[1]];
-  input.r   = keys[keyMap.r[0]] || keys[keyMap.r[1]];
-  input.l   = keys[keyMap.l[0]] || keys[keyMap.l[1]];
-  input.z   = keys[keyMap.z[0]] || keys[keyMap.z[1]];
-  input.dl  = keys[keyMap.dl[0]];
-  input.dd  = keys[keyMap.dd[0]];
-  input.dr  = keys[keyMap.dr[0]];
-  input.du  = keys[keyMap.du[0]];
-
-  if (!frameByFrame && gameMode !== 4 && gameMode !== 14) { // not in target builder, calibration screen, or frame by frame mode
-    if (input.z) {
-      if (input.lA < 0.35) {
-        input.lA = 0.35;
-      }
-      input.a = true;
-    }
-  }
-
-  if (input.l) {
-    input.lA = 1;
-  }
-  if (input.r) {
-    input.rA = 1;
-  }
-
-  return input;
-}
 
 function pollGamepadInputs( gameMode : number, gamepadInfo : GamepadInfo
                           , playerSlot : number, controllerIndex : number, frameByFrame : bool ) : Input {
